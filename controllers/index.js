9e2575ce4878377d39ceb5eca1ccf87046fbcc33
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-01-14 12:10:32 -0500

    updated index/new routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const indexRouter = express.Router();
+const User = require('../models/users')
 
+//Index
 indexRouter.get('/', (req, res) => {
-    res.render('index');
+    User.find({}, (err, allUsers) => {
+        res.render('index', {
+            users: allUsers
+        })
+    })
 });
 
+//New
+
+indexRouter.get('/add-user', (req, res) => {
+    res.render('new');
+});
+
+
+//Create
+indexRouter.post("/", (req, res) => {
+    User.create(req.body, (error, createdBook) => {
+        res.redirect('/index');
+    });
+})
 module.exports = indexRouter;
